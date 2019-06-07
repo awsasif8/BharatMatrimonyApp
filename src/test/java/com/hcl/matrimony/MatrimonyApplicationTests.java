60cd86c19ec0b58e58b759f64c9137db71a1e101
Your Name <you@example.com>
2019-06-07 17:52:05 +0530

    updating merge

--- src/test/java/com/hcl/matrimony/MatrimonyApplicationTests.java
+++ src/test/java/com/hcl/matrimony/MatrimonyApplicationTests.java
@@ -1,13 +1,9 @@
 package com.hcl.matrimony;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import com.hcl.matrimony.entity.PersonDetails;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -15,8 +11,7 @@ public class MatrimonyApplicationTests {
 
 	@Test
 	public void contextLoads() {
-		 PersonDetails person=new PersonDetails();
-		  assertThat(person.getProfileId()).isNotNull();		
+			
 	}
 
 }
--- src/test/java/com/hcl/matrimony/service/test/PersonServiceTest.java
+++ src/test/java/com/hcl/matrimony/service/test/PersonServiceTest.java
@@ -1,7 +1,6 @@
 package com.hcl.matrimony.service.test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
